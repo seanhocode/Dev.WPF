ba06f6cdd470ef1af78a0490fe69077a25b389d3
sean_ho <sean_ho@gss.com.tw>
2026-06-22 11:55:59 +0800

    [Title] [Chore] 新增 Example [Feat] - 新增 LayoutExample [Chore] - 移除多餘程式碼

---
+++ Src/Example/Layout/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Example.Layout
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Src/Example/MainWindow.xaml.cs
+++ Src/Example/MainWindow.xaml.cs
@@ -1,23 +1,17 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Example;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void OpenLayoutExampleWindow(object sender, RoutedEventArgs e)
+    {
+        var layoutExampleMainWindow = new Layout.MainWindow();
+        layoutExampleMainWindow.Show();
     }
 }
