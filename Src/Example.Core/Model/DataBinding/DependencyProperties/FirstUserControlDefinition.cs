ad817590c4dcae3e4603225d71eb41ca935d04cc
SEANHO-WORK\SeanHo <seanho2012@gmail.com>
2026-08-01 13:30:41 +0800

    * [Title] [Dependency Property] 優化範例註解 [Chore] - 優化範例註解 - 調整 Title 名稱

---
+++ Src/Example.Core/Model/DataBinding/DependencyProperties/FirstUserControlDefinition.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Example.WPF.Core.Model.DataBinding.DependencyProperties
+{
+    public class FirstUserControlDefinition
+    {
+        public static string Description = @"
+此UserControl實作一個 DependencyProperty，
+讓外部可以將值綁定至 FirstUserControlValue 屬性，
+且 FirstUserControlValue 已綁定至 UserControl 內部的 TextBlock，
+故外部綁定值至 FirstUserControlValue 後，TextBlock 會顯示綁定的值。
+";
+    }
+}
